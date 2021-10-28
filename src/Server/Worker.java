caea1cabfadf8ae74abf7aa3e5a2e4eac9e006f6
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2021-10-28 23:47:14 +0900

    Implement observer to worker

--- src/Server/Worker.java
+++ src/Server/Worker.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public abstract class Worker {
+public abstract class Worker implements Observer{
 
     private List<Workerthreads> workerthreads = new ArrayList<Workerthreads>();//List of threads that belong to worker
     private boolean running = true;
