061907f46022ed3c7ecb3942b2b0b45a442e1a2f
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2021-10-31 22:55:19 +0900

    Add Tcp_Server which contain main method

---
+++ src/Server/Tcp_Server.java
@@ -0,0 +1,13 @@
+package Server;
+
+public class Tcp_Server {
+
+    
+    public static void main(String[] args) throws Exception {
+
+        ProcessServer server = new ProcessServer(); // Instance Process server
+        server.startServer(); //Start server
+
+    }
+
+}
