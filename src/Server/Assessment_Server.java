7ae463e5c0e475553f91c3ed922cefd0ca39ca03
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2021-11-01 19:58:43 +0900

    Add class that have main method in Server

---
+++ src/Server/Assessment_Server.java
@@ -0,0 +1,16 @@
+package Server;
+/*
+  The class that include the main method which is starting point of the server
+ */
+
+public class Server {
+
+    
+    public static void main(String[] args) throws Exception {
+
+        ProcessServer server = new ProcessServer(); // Instance Process server
+        server.startServer(); //Start server
+
+    }
+
+}
